4ee1d4f0e69bbb75fe2d15237e7abdbecefce6aa
Veronica Watt <veedoubleyuh@gmail.com>
2021-09-02 02:19:33 -0700

    Added basics for localstorage.
    Created theme-override ability.

---
+++ assets/scripts/theme-updater.mjs
@@ -0,0 +1,52 @@
+import {userSettings} from "./user-settings.mjs";
+
+const overrideId = "js-color-override",
+    existingOverride = document.getElementById(overrideId),
+    newColors = document.createElement("link");
+
+/**
+ * Updates the current theme.
+ * @param update
+ */
+export default function (update = false) {
+    let href = "assets/styles/";
+
+    newColors.setAttribute("rel", "stylesheet");
+    newColors.setAttribute("id", overrideId);
+
+    // Choose overriding colors
+    if (!update) {
+        if (userSettings.getItem("theme") === "light") {
+            href = `${href}colors--light.css`;
+        }
+        else {
+            href = `${href}colors--dark.css`;
+        }
+    }
+    else {
+
+        if (userSettings.getItem("theme") === "light") {
+            href = `${href}colors--dark.css`;
+        }
+        else {
+            href = `${href}colors--light.css`;
+        }
+
+    }
+
+    newColors.setAttribute("href", href);
+
+    // Insert override colors
+    if (!existingOverride) {
+        document.head.insertAdjacentElement("beforeend", newColors);
+    }
+    else {
+        existingOverride.replaceWith(newColors);
+    }
+
+    // Update localstorage
+    if (update) {
+        userSettings.setItem("theme", (userSettings.getItem("theme") === "light" ? "dark" : "light"));
+    }
+
+}
