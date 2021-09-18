1d5cc4f9fed132b9d56a2c32e5fef72e9880a4a4
Veronica Watt <veedoubleyuh@gmail.com>
2021-09-18 13:58:29 -0700

    Fixed accidentally pulling in non-minified JS in addition to the minified version.

--- assets/scripts/theme-updater.mjs
+++ assets/scripts/theme-updater.mjs
@@ -1,4 +1,4 @@
-import {userSettings} from "/retail-plu-codes/assets/scripts/user-settings.mjs";
+import {userSettings} from "/retail-plu-codes/assets/scripts/user-settings.min.mjs";
 
 const overrideId = "js-color-override",
     existingOverride = document.getElementById(overrideId),
